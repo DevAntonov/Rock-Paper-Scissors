2465dc06548fd94830fa1d1a1bb7429c27f87bde
DevAntonov <70547557+DevAntonov@users.noreply.github.com>
2020-09-03 06:16:10 +0300

    Uploading source file with functions done

---
+++ source.py
@@ -0,0 +1,70 @@
+import tkinter
+from random import choice
+
+
+window = Tk()
+window.title("Rock, paper, scissors")
+
+choices = ["Rock", "Paper", "Scissors"]
+player_score = tkinter.IntVar()
+computer_score = tkinter.IntVar()
+result = tkinter.StringVar()
+
+
+---------------Game_functions---------------
+
+def if_rock_is_picked():
+    computer_pick = choice(choices)
+
+    if computer_pick is "Rock":
+        result.set("It's draw!")
+
+    elif computer_pick is "Paper":
+        result.set("You win!")
+        player_score.set(player_score.get() + 1)
+
+    elif computer_pick is "Scissors":
+        result.set("You lose!")
+        computer_score.set(computer_score.get() + 1)
+
+
+def if_paper_is_picked():
+    computer_pick = choice(choices)
+
+    if computer_pick is "Rock":
+        result.set("You lose!")
+        computer_score.set(computer_score.get() + 1)
+
+    elif computer_pick is "Paper":
+        result.set("It's draw!")
+
+    elif computer_pick is "Scissors":
+        result.set("You win!")
+        player_score.set(player_score.get() + 1)
+
+
+def if_scissors_is_picked():
+    computer_pick = choice(choices)
+
+    if computer_pick is "Rock":
+        result.set("You win!")
+        player_score.set(player_score.get() + 1)
+
+    elif computer_pick is "Paper":
+        result.set("You lose!")
+        computer_score.set(computer_score.get() + 1)
+
+    elif computer_pick is "Scissors":
+        result.set("It's draw!")
+
+
+
+---------------GUI---------------
+
+
+
+
+---------------Main---------------
+
+
+window.mainloop()
