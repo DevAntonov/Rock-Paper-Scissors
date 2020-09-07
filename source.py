eccb33ef30a2eea7789e69d61784a33012e606fb
DevAntonov <70547557+DevAntonov@users.noreply.github.com>
2020-09-07 04:52:35 +0300

    Update source.py

--- source.py
+++ source.py
@@ -1,14 +1,23 @@
 import tkinter
+from tkinter import *
 from random import choice
-
+from PIL import Image, ImageTk
 
 window = tkinter.Tk()
+window.geometry("880x660")
 window.title("Rock, paper, scissors")
 
 choices = ["Rock", "Paper", "Scissors"]
 player_score = tkinter.IntVar()
 computer_score = tkinter.IntVar()
 result = tkinter.StringVar()
+
+
+#_______________Images_______________
+
+rock_image = ImageTk.PhotoImage(Image.open("rock_img.jpg"))
+paper_image = ImageTk.PhotoImage(Image.open("paper_img.jpg"))
+scissors_image = ImageTk.PhotoImage(Image.open("scissors_img.jpg"))
 
 
 #---------------Game_functions---------------
@@ -61,6 +70,17 @@ def if_scissors_is_picked():
 
 #---------------GUI---------------
 
+message = Label(window, text="Pick rock, paper or scissors", font=("Arial", 20), relief=GROOVE)
+message.place(relx=0.5, rely=0.1, anchor=CENTER)
+
+rock_button = Button(window, width=230, height=230, image=rock_image, command=if_rock_is_picked(), relief=GROOVE)
+rock_button.place(relx=0.2, rely=0.35, anchor=CENTER)
+
+paper_button = Button(window, width=230, height=230, image=paper_image, command=if_paper_is_picked(), relief=GROOVE)
+paper_button.place(relx=0.5, rely=0.35, anchor=CENTER)
+
+scissors_button = Button(window, width=230, height=230, image=scissors_image, command=if_scissors_is_picked(), relief=GROOVE)
+scissors_button.place(relx=0.8, rely=0.35, anchor=CENTER)
 
 
 
