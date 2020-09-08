4fab410324f70959fef3799a9c6625a3efd0fe51
DevAntonov <70547557+DevAntonov@users.noreply.github.com>
2020-09-08 23:07:42 +0300

    Update(3)
    - Result entry added
    - Player and computer score entries added
    - Exit button added

--- source.py
+++ source.py
@@ -67,21 +67,34 @@ def if_scissors_is_picked():
         result.set("It's draw!")
 
 
+def exit_game():
+    window.destroy()
 
 #---------------GUI---------------
 
 message = Label(window, text="Pick rock, paper or scissors", font=("Arial", 20), relief=GROOVE)
 message.place(relx=0.5, rely=0.1, anchor=CENTER)
 
-rock_button = Button(window, width=230, height=230, image=rock_image, command=if_rock_is_picked(), relief=GROOVE)
+rock_button = Button(window, width=230, height=230, image=rock_image, command=if_rock_is_picked, relief=GROOVE)
 rock_button.place(relx=0.2, rely=0.35, anchor=CENTER)
 
-paper_button = Button(window, width=230, height=230, image=paper_image, command=if_paper_is_picked(), relief=GROOVE)
+paper_button = Button(window, width=230, height=230, image=paper_image, command=if_paper_is_picked, relief=GROOVE)
 paper_button.place(relx=0.5, rely=0.35, anchor=CENTER)
 
-scissors_button = Button(window, width=230, height=230, image=scissors_image, command=if_scissors_is_picked(), relief=GROOVE)
+scissors_button = Button(window, width=230, height=230, image=scissors_image, command=if_scissors_is_picked, relief=GROOVE)
 scissors_button.place(relx=0.8, rely=0.35, anchor=CENTER)
 
+win_or_lose_entry = Entry(window, textvariable=result, width=30, font=("Arial", 20), relief=GROOVE)
+win_or_lose_entry.place(relx=0.5, rely=0.7, anchor=CENTER)
+
+player_score_entry = Entry(window, textvariable=player_score, width=3, font=("Arial", 20), relief=GROOVE)
+player_score_entry.place(relx=0.3, rely=0.8, anchor=CENTER)
+
+computer_score_entry = Entry(window, textvariable=computer_score, width=3, font=("Arial", 20), relief=GROOVE)
+computer_score_entry.place(relx=0.7, rely=0.8, anchor=CENTER)
+
+exit_button = Button(window, text="Exit", width=10, height=2, command=exit_game, bg="red", activebackground="red", relief=GROOVE)
+exit_button.place(relx=0.5, rely=0.9, anchor=CENTER)
 
 
 #---------------Main---------------
