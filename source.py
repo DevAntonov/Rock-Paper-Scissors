05b610819f74a5b58ef78b1e3cb5fdd1117063ec
DevAntonov <70547557+DevAntonov@users.noreply.github.com>
2020-09-09 23:54:46 +0300

    Update source(4)
    - last round picks added
    - background color to score entry added

--- source.py
+++ source.py
@@ -8,9 +8,16 @@ window.geometry("880x660")
 window.title("Rock, paper, scissors")
 
 choices = ["Rock", "Paper", "Scissors"]
-player_score = tkinter.IntVar()
-computer_score = tkinter.IntVar()
-result = tkinter.StringVar()
+player_score = IntVar()
+computer_score = IntVar()
+result = StringVar()
+player_last_pick = StringVar()
+computer_last_pick = StringVar()
+
+score_bg_color_player = StringVar()
+score_bg_color_player.set("yellow")
+score_bg_color_computer = StringVar()
+score_bg_color_computer.set("yellow")
 
 
 #_______________Images_______________
@@ -23,7 +30,9 @@ scissors_image = ImageTk.PhotoImage(Image.open("scissors_img.jpg"))
 #---------------Game_functions---------------
 
 def if_rock_is_picked():
+    player_last_pick.set("Rock")
     computer_pick = choice(choices)
+    computer_last_pick.set(computer_pick)
 
     if computer_pick is "Rock":
         result.set("It's draw!")
@@ -35,12 +44,24 @@ def if_rock_is_picked():
     elif computer_pick is "Scissors":
         result.set("You lose!")
         computer_score.set(computer_score.get() + 1)
+
+    if player_score.get() > computer_score.get():
+        score_bg_color_player.set("green")
+        score_bg_color_computer.set("red")
+    elif player_score.get() < computer_score.get():
+        score_bg_color_player.set("red")
+        score_bg_color_computer.set("green")
+    else:
+        score_bg_color_player.set("yellow")
+        score_bg_color_computer.set("yellow")
 
 
 def if_paper_is_picked():
+    player_last_pick.set("Rock")
     computer_pick = choice(choices)
+    computer_last_pick.set(computer_pick)
 
-    if computer_pick is "Rock":
+    if computer_pick is "Paper":
         result.set("You lose!")
         computer_score.set(computer_score.get() + 1)
 
@@ -51,9 +72,20 @@ def if_paper_is_picked():
         result.set("You win!")
         player_score.set(player_score.get() + 1)
 
+    if player_score.get() > computer_score.get():
+        score_bg_color_player.set("green")
+        score_bg_color_computer.set("red")
+    elif player_score.get() < computer_score.get():
+        score_bg_color_player.set("red")
+        score_bg_color_computer.set("green")
+    else:
+        score_bg_color_player.set("yellow")
+        score_bg_color_computer.set("yellow")
 
 def if_scissors_is_picked():
+    player_last_pick.set("Scissors")
     computer_pick = choice(choices)
+    computer_last_pick.set(computer_pick)
 
     if computer_pick is "Rock":
         result.set("You win!")
@@ -66,6 +98,15 @@ def if_scissors_is_picked():
     elif computer_pick is "Scissors":
         result.set("It's draw!")
 
+    if player_score.get() > computer_score.get():
+        score_bg_color_player.set("green")
+        score_bg_color_computer.set("red")
+    elif player_score.get() < computer_score.get():
+        score_bg_color_player.set("red")
+        score_bg_color_computer.set("green")
+    else:
+        score_bg_color_player.set("yellow")
+        score_bg_color_computer.set("yellow")
 
 def exit_game():
     window.destroy()
@@ -73,7 +114,7 @@ def exit_game():
 #---------------GUI---------------
 
 message = Label(window, text="Pick rock, paper or scissors", font=("Arial", 20), relief=GROOVE)
-message.place(relx=0.5, rely=0.1, anchor=CENTER)
+message.place(relx=0.5, rely=0.05, anchor=CENTER)
 
 rock_button = Button(window, width=230, height=230, image=rock_image, command=if_rock_is_picked, relief=GROOVE)
 rock_button.place(relx=0.2, rely=0.35, anchor=CENTER)
@@ -85,13 +126,19 @@ scissors_button = Button(window, width=230, height=230, image=scissors_image, co
 scissors_button.place(relx=0.8, rely=0.35, anchor=CENTER)
 
 win_or_lose_entry = Entry(window, textvariable=result, width=30, font=("Arial", 20), relief=GROOVE)
-win_or_lose_entry.place(relx=0.5, rely=0.7, anchor=CENTER)
+win_or_lose_entry.place(relx=0.5, rely=0.6, anchor=CENTER)
 
-player_score_entry = Entry(window, textvariable=player_score, width=3, font=("Arial", 20), relief=GROOVE)
-player_score_entry.place(relx=0.3, rely=0.8, anchor=CENTER)
+player_score_entry = Entry(window, textvariable=player_score, width=3, bg=score_bg_color_player.get(), font=("Arial", 20), relief=GROOVE)
+player_score_entry.place(relx=0.2, rely=0.1, anchor=CENTER)
 
-computer_score_entry = Entry(window, textvariable=computer_score, width=3, font=("Arial", 20), relief=GROOVE)
-computer_score_entry.place(relx=0.7, rely=0.8, anchor=CENTER)
+computer_score_entry = Entry(window, textvariable=computer_score, width=3, bg=score_bg_color_computer.get(), font=("Arial", 20), relief=GROOVE)
+computer_score_entry.place(relx=0.8, rely=0.1, anchor=CENTER)
+
+player_last_pick_entry = Entry(window, textvariable=player_last_pick, width=10, font=("Arial", 20), relief=GROOVE)
+player_last_pick_entry.place(relx=0.35, rely=0.8, anchor=CENTER)
+
+computer_last_pick_entry = Entry(window, textvariable=computer_last_pick, width=10, font=("Arial", 20), relief=GROOVE)
+computer_last_pick_entry.place(relx=0.65, rely=0.8, anchor=CENTER)
 
 exit_button = Button(window, text="Exit", width=10, height=2, command=exit_game, bg="red", activebackground="red", relief=GROOVE)
 exit_button.place(relx=0.5, rely=0.9, anchor=CENTER)
